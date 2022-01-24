ed7fefdf110a067a5904ce9afdcbd06e1f0cf791
khadija-chennaoui <khadijach896@gmail.com>
2022-01-24 10:06:57 +0100

    php done

---
+++ sidebar.php
@@ -0,0 +1,59 @@
+
+
+<div class="col-3 col-md-3 col-xl-2 px-sm-2 px-0 " style="background-color: #ffffbe;">
+                <div class="d-flex flex-column align-items-center  px-3 pt-2 text-white min-vh-100">
+                 <div class="text-center" >
+                       <span class="me-3 d-sm-inline">
+                            <div class="total d-flex d-grid ">  
+                                <span class="pt-4 pb-2 mx-2 mb-3 bodr d-none  d-md-inline "> </span> 
+                                <h1 class=" mb-3  py-1 fw-bold d-none  d-md-inline  "style="color:black">E-classe</h1>
+                         </div></span>
+                        <div  class="d-grid ">
+                            <img src="https://picsum.photos/150.jpg" class="rounded-circle  ms-3 img-fluid d-none text-center  d-md-inline "  alt="...">
+                            <h2 class="text-center  d-none  d-md-inline  "style="color:black">Admin name</h2>
+                            <p class="text-center  img-fluid  d-none  d-md-inline  " style="color: darkturquoise;" >Admin</p>
+                         </div>
+                    </div>
+                    <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-md-start"id="menu">
+                       
+                        <li >
+                            <a href="indexhtml.php" class="nav-link px-0 align-middle mt-3">
+                                <i class="fs-4 bi-people"> <button type="button" class="btn btn-info    " style="color: black;">
+                                  <i class="fal fa-home-lg-alt"></i><span class=" me-5 d-none d-md-inline  "> Home</span></button>  </i></a>
+                        </li>
+                        
+                        <li>
+                            <a href="#" class="nav-link px-0 align-middle "style="color: black;" >
+                                <i class="fs-4 bi-people" >   <i class="fal fa-file-chart-line"></i></i> <span class="ms-1 d-none d-md-inline   ms-2">Course</span> </a>
+                        </li>
+                        <li>
+                            <a href="student.php" class="nav-link px-0 align-middle"style="color: black;" >
+                                <i class="fs-4 bi-people">   <i class="fal fa-graduation-cap " ></i></i> <span class="ms-1 d-none  d-md-inline   ms-2">Stydents</span> </a>
+                        </li>
+                        <li>
+                            <a href="payment.php" class="nav-link px-0 align-middle"style="color: black;" >
+                                <i class="fs-4 bi-people"> <i class="fal fa-usd-square" ></i></i> <span class="ms-1 d-none  d-md-inline ms-2">Payments</span> </a>
+                        </li>
+                        <li>
+                            <a href="#" class="nav-link px-0 align-middle"style="color: black;" >
+                                <i class="fs-4 bi-people"> <i class="fal fa-file-chart-line"></i></i> <span class="ms-1 d-none d-md-inline  ms-2">Report</span> </a>
+                        </li>
+                        <li>
+                            <a href="#" class="nav-link px-0 align-middle"style="color: black;" >
+                                <i class="fs-4 bi-people">  <i class="far fa-sliders-v-square"></i></i> <span class="ms-1 d-none  d-md-inline  ms-2">Settings</span> </a>
+                        </li>
+                        <li>
+                            <a href="Bootstrap.php" class="nav-link px-0 align-middle"style="color: black;" >
+                                 <span class="mt-5 d-none  d-md-inline  "> Logout </span> 
+                                 <i class="fs-4 bi-people"> <i class="fal fa-sign-out-alt"></i></i></a>
+                        </li>
+                    </ul>
+                    
+                   
+                </div>
+            
+
+
+
+
+
